78583f366892566488e475504015dbec1191c339
kimdonggun12 <dk8254@naver.com>
2021-12-30 22:37:19 +0900

    자기소개페이지 만들기

--- aa/single_page/views.py
+++ aa/single_page/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def landing(request):
+    return render(
+        request,
+        'single_page/landing.html'
+    )
+
+def aboutme(request):
+    return render(
+        request,
+        'single_page/aboutme.html'
+    )
