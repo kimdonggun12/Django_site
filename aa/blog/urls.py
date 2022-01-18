010d42d4f7daf726cbdd83a5c625441bad1edf80
kimdonggun12 <dk8254@naver.com>
2022-01-19 00:37:04 +0900

    tag페이지 만들기

--- aa/blog/urls.py
+++ aa/blog/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     # path('', views.index),
     path('category/<str:slug>/', views.category_page),
     path('', views.PostList.as_view()),
-    path('<int:pk>/', views.PostDetail.as_view())
-
+    path('<int:pk>/', views.PostDetail.as_view()),
+    path('tag/<str:slug>/', views.tag_page),
 
 ]
--- aa/blog/views.py
+++ aa/blog/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
-from .models import Post, Category
+from .models import Post, Category, Tag
 from django.views.generic import ListView, DetailView
 
+def tag_page(request, slug):
+    tag = Tag.objects.get(slug=slug)
+    post_list = tag.post_set.all()
 
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list': post_list,
+            'tag':tag,
+            'categories':Category.objects.all(),
+            'no_category_post_count': Post.objects.filter(category=None).count(),
+
+        }
+    )
 # Create your views here.
 class PostList(ListView):
     model = Post
