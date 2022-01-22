739bc6d46ce9186f82a4b4502283781317b17030
kimdonggun12 <dk8254@naver.com>
2022-01-23 00:05:49 +0900

    포스트 수정할때 텍스트로 태그 지정하기

--- aa/blog/urls.py
+++ aa/blog/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('<int:pk>/', views.PostDetail.as_view()),
     path('tag/<str:slug>/', views.tag_page),
     path('create_post/', views.PostCreate.as_view()),
-    path("update_post/<int:pk>", views.PostUpdate.as_view()),
+    path("update_post/<int:pk>/", views.PostUpdate.as_view()),
 
 ]
