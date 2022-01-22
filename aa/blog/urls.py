95f696c1b5a6df517928830e4b1037658b9f5b72
kimdonggun12 <dk8254@naver.com>
2022-01-22 23:44:50 +0900

    텍스트로 태그 작성하기

--- aa/blog/urls.py
+++ aa/blog/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('', views.PostList.as_view()),
     path('<int:pk>/', views.PostDetail.as_view()),
     path('tag/<str:slug>/', views.tag_page),
-    path('create_post/', views.PostCreate.as_view())
+    path('create_post/', views.PostCreate.as_view()),
+    path("update_post/<int:pk>", views.PostUpdate.as_view()),
+
 ]
--- aa/blog/views.py
+++ aa/blog/views.py
@@ -1,21 +1,59 @@
 from django.shortcuts import render, redirect
 from .models import Post, Category, Tag
-from django.views.generic import ListView, DetailView, CreateView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.core.exceptions import PermissionDenied
+from django.utils.text import slugify
 
 
+class PostUpdate(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title', 'hook_text', 'content','head_image','file_upload','category','tags']
 
-class PostCreate(LoginRequiredMixin, CreateView):
+    template_name = 'blog/post_update_form.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user == self.get_object().author:
+            return super(PostUpdate, self).dispatch(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
+
+class PostCreate(LoginRequiredMixin,UserPassesTestMixin, CreateView):
     model = Post
     fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
 
+    def test_func(self):
+        return self.request.user.is_superuser or self.request.user.is_staff
+
     def form_valid(self, form):
         current_user = self.request.user
-        if current_user.is_authenticated:
+        if current_user.is_authenticated and (current_user.is_staff or current_user.is_superuser):
             form.instance.author = current_user
-            return super(PostCreate, self).form_valid(form)
+            response = super(PostCreate, self).form_valid(form)
+
+            tags_str = self.request.POST.get('tags_str')
+            if tags_str:
+                tags_str = tags_str.strip()
+
+                tags_str = tags_str.replace(',', ';')
+                tags_list = tags_str.split(';')
+
+                for t in tags_list:
+                    t = t.strip()
+                    if t == '':
+                        continue
+                    tag, is_tag_created = Tag.objects.get_or_create(name=t)
+                    if is_tag_created:
+                        tag.slug = slugify(t, allow_unicode=True)
+                        tag.save()
+                    self.object.tags.add(tag)
+
+            return response
+
+
         else:
             return redirect('/blog/')
+
 
 def tag_page(request, slug):
     tag = Tag.objects.get(slug=slug)
