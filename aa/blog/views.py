eb238564e8e89c1f2bc22d9e81ad9eba06b57b30
kimdonggun12 <dk8254@naver.com>
2022-01-27 12:46:57 +0900

    댓글 수정 기능 추가하기

--- aa/blog/views.py
+++ aa/blog/views.py
@@ -163,6 +163,18 @@ def new_comment(request, pk):
     else:
         raise PermissionDenied
 
+class CommentUpdate(LoginRequiredMixin, UpdateView):
+
+    model = Comment
+    form_class = CommentForm
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user == self.get_object().author:
+            return super(CommentUpdate, self).dispatch(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
+
+
 
 # def index(request):
 #     posts = Post.objects.all().order_by('-pk')
